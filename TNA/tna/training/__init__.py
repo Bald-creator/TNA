5ef468e573c21e7d1aa65132f48930aa374af09d
Baldman <53111576+Bald-creator@users.noreply.github.com>
2026-02-08 01:36:13 +0800

    Delete TNA directory

--- TNA/tna/training/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Training module"""
-
-from .trainer import TNATrainer
-from .metrics import compute_classification_metrics
-
-__all__ = [
-    'TNATrainer',
-    'compute_classification_metrics',
-]
--- TNA/tna/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Utility functions"""
-
-from .helpers import count_parameters
-
-__all__ = [
-    'count_parameters',
-]
